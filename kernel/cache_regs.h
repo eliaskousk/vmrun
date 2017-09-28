d488f9d1f27c3305776da22e7be5cca2ded2903b
Elias Kouskoumvekakis <eliaskousk@gmail.com>
2017-09-28 11:12:25 +0300

    Finalize cache_regs inlines

--- kernel/cache_regs.h
+++ kernel/cache_regs.h
@@ -1,18 +1,13 @@
-#ifndef ASM_KVM_CACHE_REGS_H
-#define ASM_KVM_CACHE_REGS_H
+#ifndef VMRUN_CACHE_REGS_H
+#define VMRUN_CACHE_REGS_H
 
 #include "vmrun.h"
-
-#define KVM_POSSIBLE_CR0_GUEST_BITS X86_CR0_TS
-#define KVM_POSSIBLE_CR4_GUEST_BITS				  \
-	(X86_CR4_PVI | X86_CR4_DE | X86_CR4_PCE | X86_CR4_OSFXSR  \
-	 | X86_CR4_OSXMMEXCPT | X86_CR4_LA57 | X86_CR4_PGE)
 
 static inline unsigned long vmrun_register_read(struct vmrun_vcpu *vcpu,
 					        enum vmrun_reg reg)
 {
 	if (!test_bit(reg, (unsigned long *)&vcpu->regs_avail))
-		vmrun_x86_ops->cache_reg(vcpu, reg);
+		BUG();
 
 	return vcpu->regs[reg];
 }
@@ -36,23 +31,9 @@ static inline void vmrun_rip_write(struct vmrun_vcpu *vcpu, unsigned long val)
 	vmrun_register_write(vcpu, VCPU_REGS_RIP, val);
 }
 
-static inline u64 vmrun_pdptr_read(struct vmrun_vcpu *vcpu, int index)
-{
-	might_sleep();  /* on svm */
-
-	if (!test_bit(VCPU_EXREG_PDPTR,
-		      (unsigned long *)&vcpu->arch.regs_avail))
-		vmrun_x86_ops->cache_reg(vcpu, VCPU_EXREG_PDPTR);
-
-	return vcpu->arch.walk_mmu->pdptrs[index];
-}
-
 static inline ulong vmrun_read_cr0_bits(struct vmrun_vcpu *vcpu, ulong mask)
 {
-	ulong tmask = mask & KVM_POSSIBLE_CR0_GUEST_BITS;
-	if (tmask & vcpu->arch.cr0_guest_owned_bits)
-		vmrun_x86_ops->decache_cr0_guest_bits(vcpu);
-	return vcpu->arch.cr0 & mask;
+	return vcpu->cr0 & mask;
 }
 
 static inline ulong vmrun_read_cr0(struct vmrun_vcpu *vcpu)
@@ -62,17 +43,12 @@ static inline ulong vmrun_read_cr0(struct vmrun_vcpu *vcpu)
 
 static inline ulong vmrun_read_cr4_bits(struct vmrun_vcpu *vcpu, ulong mask)
 {
-	ulong tmask = mask & KVM_POSSIBLE_CR4_GUEST_BITS;
-	if (tmask & vcpu->arch.cr4_guest_owned_bits)
-		vmrun_x86_ops->decache_cr4_guest_bits(vcpu);
-	return vcpu->arch.cr4 & mask;
+	return vcpu->cr4 & mask;
 }
 
 static inline ulong vmrun_read_cr3(struct vmrun_vcpu *vcpu)
 {
-	if (!test_bit(VCPU_EXREG_CR3, (ulong *)&vcpu->arch.regs_avail))
-		vmrun_x86_ops->decache_cr3(vcpu);
-	return vcpu->arch.cr3;
+	return vcpu->cr3;
 }
 
 static inline ulong vmrun_read_cr4(struct vmrun_vcpu *vcpu)
@@ -88,22 +64,22 @@ static inline u64 vmrun_read_edx_eax(struct vmrun_vcpu *vcpu)
 
 static inline void enter_guest_mode(struct vmrun_vcpu *vcpu)
 {
-	vcpu->arch.hflags |= HF_GUEST_MASK;
+	vcpu->hflags |= HF_GUEST_MASK;
 }
 
 static inline void leave_guest_mode(struct vmrun_vcpu *vcpu)
 {
-	vcpu->arch.hflags &= ~HF_GUEST_MASK;
+	vcpu->hflags &= ~HF_GUEST_MASK;
 }
 
 static inline bool is_guest_mode(struct vmrun_vcpu *vcpu)
 {
-	return vcpu->arch.hflags & HF_GUEST_MASK;
+	return vcpu->hflags & HF_GUEST_MASK;
 }
 
 static inline bool is_smm(struct vmrun_vcpu *vcpu)
 {
-	return vcpu->arch.hflags & HF_SMM_MASK;
+	return vcpu->hflags & HF_SMM_MASK;
 }
 
-#endif
+#endif // VMRUN_CACHE_REGS
